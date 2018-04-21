59c051a8a4d608e8330e0817b7f6507b6692f43b
Alex Duzi <duzihd@gmail.com>
2018-04-21 16:05:42 -0300

    dev envY

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,3 @@
 module.exports = {
-  // mongoURI: 'mongodb://stephen:password@ds239128.mlab.com:39128/candidate_dev'
-  mongoURI: 'mongodb://127.0.0.1:27017/hrmanager'
+  mongoURI: 'mongodb://gupy:gupy@ds161026.mlab.com:61026/hrmanager'
 };
